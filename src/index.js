57bf63a13d1716c017c2509f7cbc714661d09afd
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-07-16 19:14:20 +0200

    Improve workflow naming

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { createDeterministApiKey } = require('./create-key')
 require('dotenv').config()
 
 const HOST = process.env.MEILI_HTTP_ADDR || 'http://localhost:7700'
-const MASTER_KEY = process.env.MEILI_MASTER_KEY || 'masterKey'
+const API_KEY = process.env.MEILI_API_KEY || 'masterKey'
 const KEY_UUID = process.env.KEY_UUID || ''
 const KEY_DESCRIPTION = process.env.KEY_DESCRIPTION || ''
 const KEY_NAME = process.env.KEY_NAME || ''
@@ -12,7 +12,7 @@ const KEY_INDEXES = process.env.KEY_INDEXES || '*'
 ;(async () => {
   await createDeterministApiKey({
     host: HOST,
-    apiKey: MASTER_KEY,
+    apiKey: API_KEY,
     keyUid: KEY_UUID,
     keyDescription: KEY_DESCRIPTION,
     keyName: KEY_NAME,
